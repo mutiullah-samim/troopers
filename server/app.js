acc5f56889fd316ad60007f4c469f430bef6262d
Mutiullah Samim <mutiullah.samim@gmail.com>
2020-04-17 18:15:28 +0800

    deploy on heroku

--- server/app.js
+++ server/app.js
@@ -10,6 +10,15 @@ app.use(cors())
 
 require('./routes')(app)
 
+// check if environment is production
+if (process.env.NODE_ENV === 'production') {
+
+	app.use(express.static(__dirname + '/public/'));
+
+	// this line is needed for handling single page application
+	app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 app.listen(config.port, () => {
 	console.log(`Server is running on port ${config.port}`)
 })
--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,11 @@
 module.exports = {
 
 	db: {
-		database: 'troopers_assesment',
-		user: 'root',
-		password: '',
+		host: 'sql7.freemysqlhosting.net',
+		database: 'sql7334087',
+		user: 'sql7334087',
+		password: 'Pn6v7c5khr',
+		port: 3306
 	},
 	port: process.env.PORT || 3000,
 	authentication: {
